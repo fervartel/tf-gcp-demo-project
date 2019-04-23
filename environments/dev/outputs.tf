272c2a232c7113fa7e9d62cdd886b13b33161bbb
Fernando Varela <fernando.varela@thomsonreuters.com>
2019-04-23 17:48:56 -0400

    autoscaler policy zones changes

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -2,3 +2,7 @@ output load_balancer_external_ip {
   description = "The external ip address of the forwarding rule (Load Balancer)."
   value       = "${module.dev-autoscaler.load_balancer_external_ip}"
 }
+
+output available_zones {
+  value       = "${module.dev-autoscaler.available_zones}"
+}
