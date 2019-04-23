4a44e4f1b68c25cd1060effd729aad4c2de451b8
Fernando Varela <fernando.varela@thomsonreuters.com>
2019-04-23 16:09:44 -0400

    Initial commit

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,4 @@
+output load_balancer_external_ip {
+  description = "The external ip address of the forwarding rule (Load Balancer)."
+  value       = "${module.dev-autoscaler.load_balancer_external_ip}"
+}
